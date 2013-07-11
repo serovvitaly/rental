e64905e7edd3758e5f7e9ba82e732f33df60433c
Vitaly Serov <serovvitaly@gmail.com>
2013-07-11 20:47:53 +0400

    работа над личным кабинетом

--- public/skins/base/js/main.js
+++ public/skins/base/js/main.js
@@ -1,9 +1,45 @@
 
+function iCheckInit(target, options){
+    if (target) target = target.trim();
+    else target = '';
+    
+    if (target && target != '') target += ' ';
+    
+    var opt = $.extend(options, {
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass:    'iradio_square-blue'
+    });
+    
+    $(target + 'input').iCheck(opt); 
+}
+
+
+function SelectBoxInit(target, options){
+    if (target) target = target.trim();
+    else target = '';
+    
+    if (target && target != '') target += ' ';
+    
+    var opt = $.extend(options, {
+        //autoWidth: true
+    });
+    
+    $(target + 'select').selectBoxIt(opt);
+    //$(target + 'select').chosen();
+}
+
+function initCustom(target){
+    iCheckInit(target);
+    //SelectBoxInit(target);
+}
+
 
 $(document).ready(function(){
    
     $('.nav *').tooltip({
         placement: 'bottom'
-    }); 
+    });
+    
+    initCustom();
     
 });
